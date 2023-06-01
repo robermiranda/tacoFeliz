c352867a542e3e7ed5cc8bf98c545cee089f8b9a
Rober Miranda <rob.mirandam@gmail.com>
2023-06-01 13:49:42 -0600

    Agregando tipos

--- src/routes/orden.ts
+++ src/routes/orden.ts
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { orden, metodoPago, ordenEstatus } = require('../datos/orden');
+const { ordenes, metodoPago, ordenEstatus } = require('../datos/orden');
 const { menu, modificadores, categoria } = require('../datos/menu');
+const { usuarios } = require('../datos/usuario');
 
 function getMenuDesc (menuId: string) {
 
@@ -17,21 +18,31 @@ function getMenuDesc (menuId: string) {
     return menuDesc;
 }
 
+function getUsuarioDesc (usuarios: any[], usuarioId: string): string {
+    const usuario: any = usuarios.find((usuario: any) => usuario.id === usuarioId);
+    if (usuario) return `${usuario.nombre} ${usuario.apPaterno}`;
+    return usuarioId;
+}
+
+
 router.get('/', function (req: any, res: any) {
     
-    const ordenResponse = orden.map((_orden: any) => {
-        const estatusDesc: string = ordenEstatus[_orden.estatus];
-        const metodoPagoDesc: string = metodoPago[_orden.metodoPago];
-        const menuDesc: any = _orden.menu.map((menuId: string) => {
+    const ordenResponse: ordenCompuestaT = ordenes.map((orden: ordenBasicaT) => {
+        const estatusDesc: string = ordenEstatus[orden.estatus];
+        const metodoPagoDesc: string = metodoPago[orden.metodoPago];
+        const menuDesc: any = orden.menu.map((menuId: string) => {
             return getMenuDesc(menuId);
         });
-
+        
         return {
-            ..._orden,
+            id: orden.id,
+            hora: orden.hora,
             estatus: estatusDesc,
             metodoPago: metodoPagoDesc,
+            usuario: getUsuarioDesc(usuarios, orden.usuario),
             menu: menuDesc,
-            modificadores: null
+            costo: orden.costo,
+            direccionEnvio: orden.direccionEnvio,
         }
     });
 
@@ -39,3 +50,31 @@ router.get('/', function (req: any, res: any) {
 });
 
 export default router;
+
+export type costoT = {
+    totalPlatillos: number,
+    totalModificadores: number,
+    subTotal: number,
+    propina: number,
+    total: number
+}
+
+export type ordenT = {
+    id: string,
+    usuario: string,
+    // hora servicio: 10 HRS. - 18 HRS
+    hora: string,
+    estatus: string,
+    costo: costoT,
+    direccionEnvio: string,
+    metodoPago: string
+}
+
+export type ordenBasicaT = ordenT & {
+    menu: string[],
+    modificadores: string[]
+}
+
+type ordenCompuestaT = ordenT & {
+    menu: any[]
+}
--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -1,16 +1,22 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
+import { usuarios, tipoUsuario, estatusUsuario } from '../datos/usuario';
+
 const router = express.Router();
-const { usuarios, tipoUsuario, estatusUsuario } = require('../datos/usuario');
 
-
-router.get('/', function (req: any, res: any) {
+router.get('/', function (req: Request, res: Response) {
     
-    const usuarioResponse = usuarios.map((usuario: any) => {
-        return {
+    const usuarioResponse: usuarioT[] = usuarios.map((usuario: usuarioT) => {
+
+        const tipo: string | undefined = tipoUsuario.get(usuario.tipo);
+        const estatus: string | undefined = estatusUsuario.get(usuario.estatus);
+
+        if (tipo && estatus) return {
             ...usuario,
-            tipo: tipoUsuario[usuario.tipo],
-            estatus: estatusUsuario[usuario.estatus],
+            tipo,
+            estatus,
         }
+
+        return usuario;
     });
     
     res.send(usuarioResponse);
@@ -18,3 +24,17 @@ router.get('/', function (req: any, res: any) {
 
 
 export default router;
+
+export type usuarioT = {
+    id: string,
+    tipo: string,
+    estatus: string,
+    nombre: string,
+    apPaterno: string,
+    apMaterno: string,
+    email: string,
+    password: string,
+    direccion?: string
+}
+
+export type tipoUsuarioT = string;
