a7be24b04dfbd3073e512c555c03ea903c2d2a3c
Rober Miranda <rob.mirandam@gmail.com>
2023-06-02 12:38:22 -0600

    Se agrega endpoint para editar un menu

--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -2,70 +2,20 @@ import express, { NextFunction, Request, Response } from 'express';
 const router = express.Router();
 const {categoria, modificadores, menu} = require('../datos/menu');
 
-
-function validaModificador (req: Request, res: Response, next: NextFunction) {
-    const {nombre, precio, disponibilidad} = req.body;
-    const _precio = Number.parseFloat(precio);
-
-    if (disponibilidad && 
-        nombre &&
-        nombre.length <= 40 &&
-        _precio &&
-        ! Number.isNaN(_precio) &&
-        _precio >= 0) {
-        
-        next();
-    }
-    else {
-        res.status(400).send({
-            status: 'warn',
-            msg: 'Son obligatorios los datos: nombre.length <= 40, precio > 0 y disponibilidad'
-        });
-    }
-}
-
-function validaModificadorParaEditar (req: Request, res: Response, next: NextFunction) {
-    const {modificadorId, nombre, precio, disponibilidad} = req.body;
-    const _precio = Number.parseFloat(precio);
-
-    if (modificadorId && (disponibilidad || 
-        (nombre && nombre.length <= 40) ||
-        _precio && ! Number.isNaN(_precio) && _precio >= 0)) {
-        
-        next();
-    }
-    else {
-        res.status(400).send({
-            status: 'warn',
-            msg: 'El modificadorId es obligatorio y almenos uno de los siguientes datos son obligatorios: nombre.length <= 40, precio > 0 y disponibilidad'
-        });
-    }
-}
-
-function validaModificadorId (req: Request, res: Response, next: NextFunction) {
-    
-    if ( ! req.params.modificadorId) {
-        res.status(400).send({
-            status: 'warn',
-            msg: 'Se debe especificar el id del modificador'
-        });
-    }
-    else {
-        const index: number = modificadores.findIndex((modificador: modificadorT) => {
-            return (modificador.id === req.params.modificadorId);
-        });
-        if (index > -1) next();
-        else {
-            res.status(400).send({
-                status: 'warn',
-                msg: 'Modificador NO encontrado'
-            });    
-        }
-    } 
-}
+const NOTAS_LONG_MAX: number = 80;
+const NOMBRE_LONG_MAX: number = 40;
+const PRECIO_MIN: number = 0;
 
 export default router.get('/', function (req: Request, res: Response) {
     // devuleve la lista de menú
+    // esta acción solo la puede ejecutar el usuario admin
+    res.send({
+        status: 'ok',
+        data: menu
+    })
+})
+.get('/', function (req: Request, res: Response) {
+    // devuleve la lista de modificadores
     // esta acción solo la puede ejecutar el usuario admin
     res.send({
         status: 'ok',
@@ -92,6 +42,36 @@ export default router.get('/', function (req: Request, res: Response) {
     res.send({
         status: 'ok',
         data: menuResponse
+    });
+})
+.post('/', validaMenu, function (req: Request, res: Response) {
+    // caso de uso (usuario admin): dar de alta un modificador
+    const {categoria, nombre, notas, precio, disponibilidad, modificadores, imagen} = req.body;
+
+    // se procede a enviar a la db el nuevo menú
+    // Se delega a la db validar la existencia de los modificadores y la validación de las categorias
+
+    res.send({
+        status: 'ok',
+        msg: 'menu agregado',
+        data: {
+            categoria, nombre, notas, precio, disponibilidad, modificadores, imagen
+        }
+    });
+})
+.patch('/', validaMenuParaEditar, function (req: Request, res: Response) {
+    // caso de uso (usuario admin): editar un menú
+    const {menuId, categoria, nombre, notas, precio, disponibilidad, modificadores, imagen} = req.body;
+
+    // se procede a enviar a la db los datos para editar el menú
+    // se da a la db la responsabilidad de verificar que existe menú para id = menuId
+
+    res.send({
+        status: 'ok',
+        msg: 'modificador agregado',
+        data: {
+            menuId, categoria, nombre, notas, precio, disponibilidad, modificadores, imagen
+        }
     });
 })
 .get('/modificadores', function (req: Request, res: Response) {
@@ -149,9 +129,130 @@ export default router.get('/', function (req: Request, res: Response) {
     });
 });
 
+// Declaración de tipos  ******************************************************
+
 type modificadorT = {
     id: string,
     nombre: string,
     precio: number,
     disponibilidad: boolean
+}
+
+
+// Funciones Auxiliares  ******************************************************
+
+function validaModificador (req: Request, res: Response, next: NextFunction) {
+    const {nombre, precio, disponibilidad} = req.body;
+    const _precio = Number.parseFloat(precio);
+
+    if (disponibilidad && 
+        nombre &&
+        nombre.length <= NOMBRE_LONG_MAX &&
+        _precio &&
+        ! Number.isNaN(_precio) &&
+        _precio >= PRECIO_MIN) {
+        
+        next();
+    }
+    else {
+        res.status(400).send({
+            status: 'warn',
+            msg: `Son obligatorios los datos: nombre.length <= ${NOMBRE_LONG_MAX}, precio > ${PRECIO_MIN} y disponibilidad`
+        });
+    }
+}
+
+function validaMenu (req: Request, res: Response, next: NextFunction) {
+    const {categoria, nombre, notas, precio, disponibilidad, modificadores} = req.body;
+    const _precio = Number.parseFloat(precio);
+
+    if (categoria &&
+        disponibilidad && 
+        nombre &&
+        nombre.length <= NOMBRE_LONG_MAX &&
+        _precio &&
+        ! Number.isNaN(_precio) &&
+        _precio >= PRECIO_MIN &&
+        ( ! notas || (notas && notas.length <= NOTAS_LONG_MAX)) &&
+        (! modificadores || (Array.isArray(modificadores) && modificadores.length > 0))) {
+        
+        next();
+    }
+    else {
+        res.status(400).send({
+            status: 'warn',
+            msg: `Son obligatorios los datos:
+            nombre.length <= ${NOMBRE_LONG_MAX},
+            precio > ${PRECIO_MIN},
+            disponibilidad,
+            lista de modificadores validos,
+            categoria`
+        });
+    }
+}
+
+function validaModificadorParaEditar (req: Request, res: Response, next: NextFunction) {
+    const {modificadorId, nombre, precio, disponibilidad} = req.body;
+    const _precio = Number.parseFloat(precio);
+
+    if ((nombre || _precio || disponibilidad) &&
+        modificadorId &&
+        ( ! nombre || nombre.length <= NOMBRE_LONG_MAX) &&
+        ( ! precio || ( ! Number.isNaN(_precio) && _precio >= PRECIO_MIN))) {
+
+        next();
+    }
+    else {
+        res.status(400).send({
+            status: 'warn',
+            msg: `El modificadorId es obligatorio y almenos uno de los siguientes datos son obligatorios: nombre.length <= ${NOMBRE_LONG_MAX}, precio > ${PRECIO_MIN} y disponibilidad`
+        });
+    }
+}
+
+function validaMenuParaEditar (req: Request, res: Response, next: NextFunction) {
+    const {menuId, categoria, nombre, notas, precio, disponibilidad, modificadores} = req.body;
+    const _precio = Number.parseFloat(precio);
+
+    if ((categoria || disponibilidad || nombre || _precio || notas || modificadores) && 
+        menuId &&
+        ( ! precio || (! Number.isNaN(_precio) && _precio >= PRECIO_MIN)) &&
+        ( ! notas || notas.length <= NOTAS_LONG_MAX) &&
+        ( ! modificadores || (Array.isArray(modificadores) && modificadores.length > 0))) {
+
+        next();
+    }
+    else {
+        res.status(400).send({
+            status: 'warn',
+            msg: `Almenos uno de los siguientes datos son obligatorios:
+            nombre.length <= ${NOMBRE_LONG_MAX},
+            precio > ${PRECIO_MIN},
+            disponibilidad,
+            lista de modificadores validos,
+            categoria`
+        });
+    }
+}
+
+function validaModificadorId (req: Request, res: Response, next: NextFunction) {
+    
+    if ( ! req.params.modificadorId) {
+        res.status(400).send({
+            status: 'warn',
+            msg: 'Se debe especificar el id del modificador'
+        });
+    }
+    else {
+        const index: number = modificadores.findIndex((modificador: modificadorT) => {
+            return (modificador.id === req.params.modificadorId);
+        });
+        if (index > -1) next();
+        else {
+            res.status(400).send({
+                status: 'warn',
+                msg: 'Modificador NO encontrado'
+            });    
+        }
+    } 
 }
