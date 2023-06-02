ba187b7d7edfe38255ddeadea77c74a5bf3ec07d
Rober Miranda <rob.mirandam@gmail.com>
2023-06-02 07:11:42 -0600

    Agregando logica para bloquear usuario

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -17,7 +17,27 @@ function validaUsuario (req: Request, res: Response, next: NextFunction) {
 
 const router = express.Router();
 
-router.get('/', function (req: Request, res: Response) {
+function validaUsuarioEmail (req: Request, res: Response, next: NextFunction) {
+    
+    if ( ! req.params.usuarioEmail) {
+        res.status(400).send({
+            status: 'warn',
+            msg: 'Se debe especificar el email del usuario'
+        });
+    }
+    else {
+        const index: number = usuarios.findIndex((usuario: usuarioT) => usuario.email === req.params.usuarioEmail);
+        if (index > -1) next();
+        else {
+            res.status(400).send({
+                status: 'warn',
+                msg: 'usuario NO encontrado'
+            });    
+        }
+    }   
+}
+
+export default router.get('/', function (req: Request, res: Response) {
     
     const usuarioResponse: usuarioT[] = usuarios.map((usuario: usuarioT) => {
 
@@ -62,10 +82,20 @@ router.get('/', function (req: Request, res: Response) {
             usuarioEstatus: 'activo'
         }
     });
+})
+.delete('/:usuarioEmail', validaUsuarioEmail, function (req: Request, res: Response) {
+    // caso de uso (usuario admin): bloquear usuario
+
+    // se procede a llamar al endpoint de la base de datos para cancelar al usuario
+    // para lo cual se debe proporcionar dos parametros:
+    // email del usuario a eliminar y también el usuario del admin
+    //  recuerdese que solo el usuario admin puede bloquear usuarios
+
+    res.send({
+        status: 'ok',
+        msg: 'se procede a bloquear al usuario'
+    });
 });
-
-
-export default router;
 
 export type usuarioT = {
     id?: string,
