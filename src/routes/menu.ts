d933bb1e09952f0efc2f0a6078bbaa4e8db5d663
Rober Miranda <rob.mirandam@gmail.com>
2023-06-02 07:31:42 -0600

    Agregando ruta para ver lista de modificadores

--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -2,9 +2,16 @@ import express, { NextFunction, Request, Response } from 'express';
 const router = express.Router();
 const {categoria, modificadores, menu} = require('../datos/menu');
 
-
 export default router.get('/', function (req: Request, res: Response) {
-    // caso de uso: ver menú
+    // devuleve la lista de menú
+    // esta acción solo la puede ejecutar el usuario admin
+    res.send({
+        status: 'ok',
+        data: menu
+    })
+})
+.get('/detalle', function (req: Request, res: Response) {
+    // caso de uso (usuario final): ver menú
     const menuResponse = menu.map((_menu: any) => {
         const categoriaDesc = categoria[_menu.categoria];
         
@@ -20,5 +27,16 @@ export default router.get('/', function (req: Request, res: Response) {
 
     });
 
-    res.send(menuResponse);
+    res.send({
+        status: 'ok',
+        data: menuResponse
+    });
+})
+.get('/modificadores', function (req: Request, res: Response) {
+    // devuleve la lista de modificadores
+    // esta acción solo la puede ejecutar el usuario admin
+    res.send({
+        status: 'ok',
+        data: modificadores
+    })
 });
