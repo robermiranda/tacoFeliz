9b5cef636be2ba465b8c31e8287aafd1c0826c4e
Rober Miranda <rob.mirandam@gmail.com>
2023-06-02 23:46:04 -0600

    Conectando router usuario con mongodb atlas

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -20,44 +20,45 @@ export default router.get('/', function (req: Request, res: Response) {
     });
 })
 .post('/', validaUsuario, function (req: Request, res: Response) {
-    // Caso de uso: Rregistro en aplicación
-    const nombre: string = req.body.nombre;
-    const apPaterno: string = req.body.apPaterno;
-    const apMaterno: string = req.body.apMaterno;
-    const email: string = req.body.email;
-    const password: string = req.body.password;
-
-    // Se procede a registrar el usuario
-    const usuarioResponse: usuarioT = {
-        id: 'user-123',
-        tipo: '2',
-        estatus: 'activo',
-        nombre,
-        apPaterno,
-        apMaterno,
-        email,
-        password: '*****'
-    }
-
-    res.send({
-        status: 'ok',
-        msg: 'usuario registrado',
-        data: {
-            usuarioEstatus: 'activo'
-        }
+    
+    axios.post(URL_BASE, {
+        nombre: req.body.nombre,
+        apPaterno: req.body.apPaterno,
+        apMaterno: req.body.apMaterno,
+        email:  req.body.email,
+        password: req.body.password    
+    })
+    .then(response => {
+        res.send(response.data);
+    })
+    .catch(err => {
+        console.error('ERROR al registrar usuario', req.body.nombre);
+        res.status(500).send({
+            status: 'error',
+            msg: 'usuario NO registrado'
+        });
     });
 })
-.delete('/:usuarioEmail', validaUsuarioEmail, function (req: Request, res: Response) {
+.patch('/', function (req: Request, res: Response) {
     // caso de uso (usuario admin): bloquear usuario
 
     // se procede a llamar al endpoint de la base de datos para cancelar al usuario
     // para lo cual se debe proporcionar dos parametros:
     // email del usuario a eliminar y también el usuario del admin
     //  recuerdese que solo el usuario admin puede bloquear usuarios
-
-    res.send({
-        status: 'ok',
-        msg: 'se procede a bloquear al usuario'
+    axios.patch(URL_BASE, {
+        emailUsuario: req.body.emailUsuario,
+        emailAdmin: req.body.emailAdmin
+    })
+    .then(response => {
+        res.send(response.data);
+    })
+    .catch(err => {
+        console.error('ERROR al bloquear usuario', req.body.nombre);
+        res.status(500).send({
+            status: 'error',
+            msg: 'usuario NO bloqueado'
+        });
     });
 });
 
@@ -78,33 +79,13 @@ export type tipoUsuarioT = string;
 function validaUsuario (req: Request, res: Response, next: NextFunction) {
     const nombre: string = req.body.nombre;
     const apPaterno: string = req.body.apPaterno;
-    const apMaterno: string = req.body.apMaterno;
     const email: string = req.body.email;
     const password: string = req.body.password;
 
-    if (nombre && email && password) next();
+    if (nombre && apPaterno && email && password) next();
     else res.status(400).send({
         status: 'warn',
-        msg: 'Los siguientes datos son obligatorios: nombre, correo electronico y contraseña'
+        msg: 'Los siguientes datos son obligatorios: nombre, apPaterno, correo electronico y contraseña'
     });
 }
 
-function validaUsuarioEmail (req: Request, res: Response, next: NextFunction) {
-    
-    if ( ! req.params.usuarioEmail) {
-        res.status(400).send({
-            status: 'warn',
-            msg: 'Se debe especificar el email del usuario'
-        });
-    }
-    else {
-        const index: number = usuarios.findIndex((usuario: usuarioT) => usuario.email === req.params.usuarioEmail);
-        if (index > -1) next();
-        else {
-            res.status(400).send({
-                status: 'warn',
-                msg: 'usuario NO encontrado'
-            });    
-        }
-    }   
-}
