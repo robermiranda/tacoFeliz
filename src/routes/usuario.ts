7e9604455ad4378875a6feb5e1712c71c57f55e4
Rober Miranda <rob.mirandam@gmail.com>
2023-06-02 22:59:56 -0600

    Obteniendo usuarios de Mongodb Atlas

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -1,59 +1,23 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { usuarios, tipoUsuario, estatusUsuario } from '../datos/usuario';
+import axios from 'axios';
 
-function validaUsuario (req: Request, res: Response, next: NextFunction) {
-    const nombre: string = req.body.nombre;
-    const apPaterno: string = req.body.apPaterno;
-    const apMaterno: string = req.body.apMaterno;
-    const email: string = req.body.email;
-    const password: string = req.body.password;
-
-    if (nombre && email && password) next();
-    else res.status(400).send({
-        status: 'warn',
-        msg: 'Los siguientes datos son obligatorios: nombre, correo electronico y contraseña'
-    });
-}
 
 const router = express.Router();
-
-function validaUsuarioEmail (req: Request, res: Response, next: NextFunction) {
-    
-    if ( ! req.params.usuarioEmail) {
-        res.status(400).send({
-            status: 'warn',
-            msg: 'Se debe especificar el email del usuario'
-        });
-    }
-    else {
-        const index: number = usuarios.findIndex((usuario: usuarioT) => usuario.email === req.params.usuarioEmail);
-        if (index > -1) next();
-        else {
-            res.status(400).send({
-                status: 'warn',
-                msg: 'usuario NO encontrado'
-            });    
-        }
-    }   
-}
+const URL_BASE = 'https://us-east-1.aws.data.mongodb-api.com/app/taco-feliz-uaepm/endpoint/usuarios';
 
 export default router.get('/', function (req: Request, res: Response) {
-    
-    const usuarioResponse: usuarioT[] = usuarios.map((usuario: usuarioT) => {
-
-        const tipo: string | undefined = tipoUsuario.get(usuario.tipo);
-        const estatus: string | undefined = estatusUsuario.get(usuario.estatus);
-
-        if (tipo && estatus) return {
-            ...usuario,
-            tipo,
-            estatus,
-        }
-
-        return usuario;
+    axios(URL_BASE)
+    .then(usuarios => {
+        res.send(usuarios.data);
+    })
+    .catch(err => {
+        console.error('ERROR al obtener los usuarios', err.message);
+        res.status(500).send({
+            status: 'error',
+            msg: 'ERROR al obtener los usuarios'
+        })
     });
-    
-    res.send(usuarioResponse);
 })
 .post('/', validaUsuario, function (req: Request, res: Response) {
     // Caso de uso: Rregistro en aplicación
@@ -109,3 +73,38 @@ export type usuarioT = {
 }
 
 export type tipoUsuarioT = string;
+
+
+function validaUsuario (req: Request, res: Response, next: NextFunction) {
+    const nombre: string = req.body.nombre;
+    const apPaterno: string = req.body.apPaterno;
+    const apMaterno: string = req.body.apMaterno;
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+
+    if (nombre && email && password) next();
+    else res.status(400).send({
+        status: 'warn',
+        msg: 'Los siguientes datos son obligatorios: nombre, correo electronico y contraseña'
+    });
+}
+
+function validaUsuarioEmail (req: Request, res: Response, next: NextFunction) {
+    
+    if ( ! req.params.usuarioEmail) {
+        res.status(400).send({
+            status: 'warn',
+            msg: 'Se debe especificar el email del usuario'
+        });
+    }
+    else {
+        const index: number = usuarios.findIndex((usuario: usuarioT) => usuario.email === req.params.usuarioEmail);
+        if (index > -1) next();
+        else {
+            res.status(400).send({
+                status: 'warn',
+                msg: 'usuario NO encontrado'
+            });    
+        }
+    }   
+}
