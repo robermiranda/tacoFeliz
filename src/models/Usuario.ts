a0408845d680be846020d94136e071e62a5c9cfc
Rober Miranda <rob.mirandam@gmail.com>
2023-06-06 20:43:20 -0600

    Se agrega el modelo Usuario

---
+++ src/models/Usuario.ts
@@ -0,0 +1,53 @@
+import { Schema, model } from "mongoose";
+
+export const TIPO_USUARIO = {
+    final: 'USUARIO_FINAL',
+    admin: 'SUPER_ADMIN'
+}
+
+export const ESTATUS_USUARIO = {
+    activo: 'ACTIVO',
+    bloqueado: 'BLOQUEADO'
+}
+
+const usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        maxLength: 40,
+        required: true,
+    },
+    apPaterno: {
+        type: String,
+        maxLength: 40
+    },
+    apMaterno: {
+        type: String,
+        maxLength: 40
+    },
+    email: {
+        type: String,
+        required: true,
+        maxLength: 40,
+        index: true,
+        unique: true
+    },
+    password: {
+        required: true,
+        type: String,
+        maxLength: 40
+    },
+    estatus: {
+        type: String,
+        required: true,
+        default: ESTATUS_USUARIO.activo,
+        maxLength: 20
+    },
+    tipo: {
+        type: String,
+        required: true,
+        default: TIPO_USUARIO.final,
+        maxLength: 20
+    }
+});
+
+export const Usuario = model ("Usuario", usuarioSchema);
