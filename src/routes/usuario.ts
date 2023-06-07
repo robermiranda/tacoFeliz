dde5b6f2b40597f2a7af7ac3b8ad5808a1c0846f
Rober Miranda <rob.mirandam@gmail.com>
2023-06-06 22:38:34 -0600

    Agregando uso del modelo Usuario

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -1,34 +1,20 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Usuario, ESTATUS_USUARIO } from '../models/Usuario';
+import { stdRes } from '../util';
 
 
-const TIPO_USUARIO = {
-    final: 'FINAL',
-    admin: 'SUPER_ADMIN'
-}
-const ESTATUS = {
-    activo: 'ACTIVO',
-    bloquedao: 'BLOQUEADO'
-}
-const prisma = new PrismaClient();
 const router = express.Router();
 
-
 export default router.get('/', async function (req: Request, res: Response) {
+    // usuario admin
+    // obtiene la lista de usuarios
+    
     try {
-        const usuarios = await prisma.usuarios.findMany();
-        res.send({
-            status: 'ok',
-            msg: `usuarios obtenidos: ${usuarios.length}`,
-            data: usuarios
-        });
+        const usuarios = await Usuario.find({});
+        res.send(stdRes('ok', `usuarios obtenidos: ${usuarios.length}`, usuarios));
     }
     catch (err) {
-        console.error('ERROR al obtener todos los usuarios', err);
-        res.status(500).send({
-            status: 'error',
-            msg: 'ERROR al obtener los usuarios'
-        });
+        res.status(500).send(stdRes('error','ERROR al obtener los usuarios'));
     }
 })
 .post('/', validaUsuario, async function (req: Request, res: Response) {
@@ -36,71 +22,52 @@ export default router.get('/', async function (req: Request, res: Response) {
     // caso de uso: Registro en aplicación
 
     const usuario: usuarioT = {
-        email: req.body.email,
-        tipo: TIPO_USUARIO.final,
-        estatus: ESTATUS.activo,
-        password: req.body.password,
         nombre: req.body.nombre,
+        email: req.body.email,
+        password: req.body.password,
     }
 
     if (req.body.apPaterno) usuario.apPaterno = req.body.apPaterno;
     if (req.body.apMaterno) usuario.apMaterno = req.body.apMaterno;
 
     try {
-        await prisma.usuarios.create({
-            data: usuario
-        });
-
-        res.send({
-            status: 'ok'
-        });
+        const _usuario = await Usuario.create(usuario);
+        res.send(stdRes('ok', `usuario creado con id: ${_usuario._id}`));
     }
     catch (err) {
         console.error('ERROR al insertar usuario', usuario.email, err);
-        res.status(500).send({
-            status: 'error',
-            msg: 'usuario NO registrado'
-        });
+        res.status(500).send(stdRes('error', 'usuario NO registrado'));
     }
 })
-.patch('/', async function (req: Request, res: Response) {
-    // caso de uso (usuario admin): bloquear usuario
-
-    // se procede a llamar al endpoint de la base de datos para cancelar al usuario
-    // para lo cual se debe proporcionar dos parametros:
-    // email del usuario a eliminar: emailUsuario
-    // recuerdese que solo el usuario admin puede bloquear usuarios
+.patch('/estatus/', async function (req: Request, res: Response) {
+    // usuario admin
+    // caso de uso: bloquear usuario
+    // se debe proporcionar el email del usuario a bloquear
 
     try {
-        const bloqueado = await prisma.usuarios.update({
-            where: {
-                email: req.body.emailUsuario
-            },
-            data: {
-                estatus: ESTATUS.bloquedao
-            }
-        });
+        const response = await Usuario.updateOne(
+            {email: req.body.emailUsuario},
+            {estatus: ESTATUS_USUARIO.bloqueado}
+        );
 
-        res.send({
-            status: 'ok',
-            msg: 'usuario bloqueado'
-        });
+        let stdres;
+        if (response.modifiedCount === 1) stdres = stdRes('ok', 'usuario bloqueado');
+        else stdres = stdRes('warn', 'usuario NO bloqueado');
+
+        res.send(stdres);
     }
     catch (err) {
         const msg = `ERROR al bloquear usuario ${req.body.emailUsuario}`;
         console.error(msg, err);
-        res.status(500).send({
-            status: 'error',
-            msg
-        });
+        res.status(500).send(stdRes('error', msg));
     }
 });
 
 
 export type usuarioT = {
     id?: string,
-    tipo: string,
-    estatus: string,
+    tipo?: string,
+    estatus?: string,
     nombre: string,
     apPaterno?: string,
     apMaterno?: string,
@@ -125,4 +92,3 @@ function validaUsuario (req: Request, res: Response, next: NextFunction) {
         msg: 'Los siguientes datos son obligatorios: nombre, apPaterno, correo electronico y contraseña'
     });
 }
-
